851b1aae2cd67cb34bedf8894baf44d399648357
mahammad <mhmmdrzyv@gmail.com>
2024-11-05 01:36:44 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./index.scss"
 import { Components } from "@/pages/Components.tsx"
 import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "@/pages/LoginPage.tsx"
 import { RegisterPage } from "@/pages/RegisterPage.tsx"
 import { SetupPage } from "@/pages/SetupPage.tsx"
 import { MainPage } from "@/pages/MainPage.tsx"
+import { LoginPage2 } from "@/pages/LoginPage2.tsx"
 
 function App() {
     return (
         <Routes>
             <Route path={"/"}>
                 <Route path={""} element={<Navigate to={"login"} />} />
-                <Route path={"login"} element={<LoginPage />} />
+                <Route path={"login"} element={<LoginPage2 />} />
                 <Route path={"register"} element={<RegisterPage />} />
                 <Route path={"setup"} element={<SetupPage />} />
                 <Route path={"main"} element={<MainPage />} />
